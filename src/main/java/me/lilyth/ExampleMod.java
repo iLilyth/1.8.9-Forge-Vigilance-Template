d55c5ba74513492dd0fe4f1802c6c63a99c25bac
iLilyth <lilyfromsandbox@gmail.com>
2023-03-31 20:35:29 -0400

    Initial Upload

--- src/main/java/me/lilyth/ExampleMod.java
+++ src/main/java/me/lilyth/ExampleMod.java
@@ -1,7 +1,5 @@
 package me.lilyth;
 
-import me.lilyth.config.ExampleCommand;
-import me.lilyth.config.ExampleConfiguration;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.*;
@@ -16,38 +14,31 @@ import net.minecraftforge.fml.common.event.*;
 public class ExampleMod {
     public static final String MODID = "examplemod";
     public static final String MOD_NAME = "ExampleMod";
-
     public static final String VERSION = "1.0";
     public static final String MC_VERSION = "1.8.9";
     public static ExampleMod instance;
-    public static ExampleConfiguration exampleConfiguration;
+
     public ExampleMod() {
-        instance=this;
-        exampleConfiguration = new ExampleConfiguration();
+        instance = this;
     }
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
+
     }
+
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        // Initialize and preload modules
-        exampleConfiguration.preload();
-        new ExampleCommand("dsnf").register();
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event){
-        System.out.println("DSNE successfully Initialized!");
+    public void postInit(FMLPostInitializationEvent event) {
+        System.out.println("Mod has been successfully Initialized!");
     }
-    /////
 
     public static ExampleMod getInstance() {
         return instance;
-    }
-    public static ExampleConfiguration getConfig() {
-        return exampleConfiguration;
     }
 }
